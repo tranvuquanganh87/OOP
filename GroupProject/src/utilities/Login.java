5da1b483df2871fc4eb393c87ccd27e655714a54
Quốc Đoàn <huuquoc7603@gmail.com>
2023-05-19 14:26:59 +0700

    ok

--- GroupProject/src/utilities/Login.java
+++ GroupProject/src/utilities/Login.java
@@ -1,18 +1,18 @@
 package utilities;
 import code.*;
 public class Login {
-	public static boolean LoginValidation (String username, String password) {
+	public static Account LoginValidation (String username, String password) {
 		for (Account i : VideoStore.getAccountList()) {
 			if (username.equals(i.getUsername())) {
 				if (password.equals(i.getPassword())) {
-					return true;
+					return i;
 				} else {
 					System.out.println("Incorrect password!");
-					return false;
+					return null;
 				}
 			}
 		}
 		System.out.println("Invalid username!");
-		return false;
+		return null;
 	}
 }
